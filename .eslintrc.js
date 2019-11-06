03fd342f8e2e9bef3dc886ae632d9c69075fc48d
igorashs <igorashsirbu@gmail.com>
2019-11-06 20:44:21 +0200

    change arrow-parens rule to always

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,23 +3,18 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-  ],
+  extends: ['plugin:vue/essential', '@vue/airbnb'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'arrow-parens': ['error', 'always'],
   },
   parserOptions: {
     parser: 'babel-eslint',
   },
   overrides: [
     {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
-      ],
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
       env: {
         jest: true,
       },
