f23042cd3dd032b3fd5f3aa4a82c9cb841017663
igorashs <igorashsirbu@gmail.com>
2019-11-11 10:53:05 +0200

    add spec for whether or not all ships have been sunk

--- src/scripts/factories/__tests__/createGameBoard.spec.js
+++ src/scripts/factories/__tests__/createGameBoard.spec.js
@@ -8,20 +8,26 @@ import createShip from '../createShip';
 
 // valid format for x and y: [0 - 9]
 
-// todo report whether or not all ships have been sunk
-
 describe('createGameBoard', () => {
   it('create a gameBoard with no ships', () => {
-    expect(createGameBoard().isReady()).toBe(false);
+    const gameBoard = createGameBoard();
+
+    expect(gameBoard.isReady()).toBe(false);
+    expect(gameBoard.isAllShipsSunk()).toBe(false);
+    expect(gameBoard.getAliveShipsCount()).toBe(0);
   });
 
-  it('return true if the ship was placed at A5', () => {
+  it('return true if the ship was placed at A5 and counted as alive', () => {
+    const gameBoard = createGameBoard();
     const ship = createShip({ length: 1 });
-    expect(createGameBoard().placeShipAt(ship, { x: 0, y: 5, position: 'vertical' })).toBe(true);
+
+    expect(gameBoard.placeShipAt(ship, { x: 0, y: 5, position: 'vertical' })).toBe(true);
+    expect(gameBoard.getAliveShipsCount()).toBe(1);
   });
 
   it('return false if cords are not in a valid format', () => {
     const ship = createShip({ length: 1 });
+
     expect(createGameBoard().placeShipAt(ship, { x: 'D', y: 'c', position: 'x' })).toBe(false);
     expect(createGameBoard().placeShipAt(ship, { x: '4', y: 4, position: 'x' })).toBe(false);
     expect(createGameBoard().placeShipAt(ship, { x: 4, y: '4', position: 'x' })).toBe(false);
@@ -29,6 +35,7 @@ describe('createGameBoard', () => {
 
   it('return false if cords are not in a valid range', () => {
     const ship = createShip({ length: 3 });
+
     expect(createGameBoard().placeShipAt(ship, { x: 10, y: -12, position: 'vertical' })).toBe(
       false,
     );
@@ -36,6 +43,7 @@ describe('createGameBoard', () => {
 
   it('position works with shorthands and is case insensitive', () => {
     const ship = createShip({ length: 1 });
+
     expect(createGameBoard().placeShipAt(ship, { x: 1, y: 0, position: 'v' })).toBe(true);
     expect(createGameBoard().placeShipAt(ship, { x: 0, y: 0, position: 'X' })).toBe(true);
     expect(createGameBoard().placeShipAt(ship, { x: 4, y: 0, position: 'veErTiCal' })).toBe(true);
@@ -43,18 +51,21 @@ describe('createGameBoard', () => {
 
   it('return false if position is not valid', () => {
     const ship = createShip({ length: 1 });
+
     expect(createGameBoard().placeShipAt(ship, { x: 1, y: 0, position: 'diagonal' })).toBe(false);
   });
 
   it('work with default horizontal position if is not defined', () => {
     const ship = createShip({ length: 2 });
     const gameBoard = createGameBoard();
+
     expect(gameBoard.placeShipAt(ship, { x: 0, y: 0 })).toBe(true);
     expect(gameBoard.receiveAttack({ x: 0, y: 1 })).toBe(true);
   });
 
   it('return false if cords are not specified', () => {
     const ship = createShip({ length: 3 });
+
     expect(createGameBoard().placeShipAt(ship)).toBe(false);
     expect(createGameBoard().placeShipAt(ship, {})).toBe(false);
     expect(createGameBoard().placeShipAt(ship, { x: 2 })).toBe(false);
@@ -90,12 +101,14 @@ describe('createGameBoard', () => {
 
   it('it can be only 1 ship with length = 4', () => {
     const gameBoard = createGameBoard();
+
     expect(gameBoard.placeShipAt(createShip({ length: 4 }), { x: 1, y: 1 })).toBe(true);
     expect(gameBoard.placeShipAt(createShip({ length: 4 }), { x: 6, y: 6 })).toBe(false);
   });
 
   it('it can be only 2 ships with length = 3', () => {
     const gameBoard = createGameBoard();
+
     expect(gameBoard.placeShipAt(createShip({ length: 3 }), { x: 0, y: 0 })).toBe(true);
     expect(gameBoard.placeShipAt(createShip({ length: 3 }), { x: 0, y: 2 })).toBe(true);
     expect(gameBoard.placeShipAt(createShip({ length: 3 }), { x: 0, y: 4 })).toBe(false);
@@ -103,6 +116,7 @@ describe('createGameBoard', () => {
 
   it('it can be only 3 ships with length = 2', () => {
     const gameBoard = createGameBoard();
+
     expect(gameBoard.placeShipAt(createShip({ length: 2 }), { x: 0, y: 0 })).toBe(true);
     expect(gameBoard.placeShipAt(createShip({ length: 2 }), { x: 0, y: 2 })).toBe(true);
     expect(gameBoard.placeShipAt(createShip({ length: 2 }), { x: 0, y: 4 })).toBe(true);
@@ -111,6 +125,7 @@ describe('createGameBoard', () => {
 
   it('it can be only 4 ships with length = 1', () => {
     const gameBoard = createGameBoard();
+
     expect(gameBoard.placeShipAt(createShip({ length: 1 }), { x: 0, y: 0 })).toBe(true);
     expect(gameBoard.placeShipAt(createShip({ length: 1 }), { x: 0, y: 2 })).toBe(true);
     expect(gameBoard.placeShipAt(createShip({ length: 1 }), { x: 0, y: 4 })).toBe(true);
@@ -120,6 +135,7 @@ describe('createGameBoard', () => {
 
   it('it can be 10 ships only on a board', () => {
     const gameBoard = createGameBoard();
+
     expect(gameBoard.isReady()).toBe(false);
     expect(gameBoard.placeShipAt(createShip({ length: 4 }), { x: 0, y: 0 })).toBe(true);
     expect(gameBoard.placeShipAt(createShip({ length: 3 }), { x: 0, y: 2 })).toBe(true);
@@ -160,6 +176,7 @@ describe('createGameBoard', () => {
 
   it('place ships on random cords on board', () => {
     const gameBoard = createGameBoard();
+
     expect(gameBoard.placeShipRandom(createShip({ length: 4 }))).toBe(true);
     expect(gameBoard.isReady()).toBe(false);
     expect(gameBoard.placeShipRandom(createShip({ length: 3 }))).toBe(true);
@@ -172,6 +189,8 @@ describe('createGameBoard', () => {
     expect(gameBoard.placeShipRandom(createShip({ length: 1 }))).toBe(true);
     expect(gameBoard.placeShipRandom(createShip({ length: 1 }))).toBe(true);
     expect(gameBoard.isReady()).toBe(true);
+    expect(gameBoard.isAllShipsSunk()).toBe(false);
+    expect(gameBoard.getAliveShipsCount()).toBe(10);
   });
 
   it('can take hits if board is ready and return true if is hit | false if is missed', () => {
@@ -190,10 +209,10 @@ describe('createGameBoard', () => {
     expect(gameBoard.placeShipAt(createShip({ length: 3 }), { x: 0, y: 4 })).toBe(true);
     expect(gameBoard.placeShipAt(createShip({ length: 2 }), { x: 0, y: 6 })).toBe(true);
     expect(gameBoard.placeShipAt(createShip({ length: 2 }), { x: 0, y: 8 })).toBe(true);
-    expect(gameBoard.placeShipAt(createShip({ length: 2 }), { x: 5, y: 0 })).toBe(true);
-    expect(gameBoard.placeShipAt(createShip({ length: 1 }), { x: 5, y: 2 })).toBe(true);
-    expect(gameBoard.placeShipAt(createShip({ length: 1 }), { x: 5, y: 4 })).toBe(true);
-    expect(gameBoard.placeShipAt(createShip({ length: 1 }), { x: 5, y: 6 })).toBe(true);
+    expect(gameBoard.placeShipAt(createShip({ length: 2 }), { x: 6, y: 0 })).toBe(true);
+    expect(gameBoard.placeShipAt(createShip({ length: 1 }), { x: 6, y: 2 })).toBe(true);
+    expect(gameBoard.placeShipAt(createShip({ length: 1 }), { x: 6, y: 4 })).toBe(true);
+    expect(gameBoard.placeShipAt(createShip({ length: 1 }), { x: 6, y: 6 })).toBe(true);
 
     expect(gameBoard.receiveAttack({ x: 0, y: 0 })).toEqual({
       ship4: 0,
@@ -203,9 +222,65 @@ describe('createGameBoard', () => {
       message: 'Board needs 1 more ship',
     });
 
-    expect(gameBoard.placeShipAt(createShip({ length: 1 }), { x: 5, y: 8 })).toBe(true);
+    expect(gameBoard.placeShipAt(createShip({ length: 1 }), { x: 6, y: 8 })).toBe(true);
 
     expect(gameBoard.receiveAttack({ x: 0, y: 0 })).toBe(true);
     expect(gameBoard.receiveAttack({ x: 9, y: 0 })).toBe(false);
+  });
+
+  it('return true if all ships have been sunk and alive ships should be 0', () => {
+    const gameBoard = createGameBoard();
+
+    expect(gameBoard.placeShipAt(createShip({ length: 4 }), { x: 0, y: 0 })).toBe(true);
+    expect(gameBoard.placeShipAt(createShip({ length: 3 }), { x: 0, y: 2 })).toBe(true);
+    expect(gameBoard.placeShipAt(createShip({ length: 3 }), { x: 0, y: 4 })).toBe(true);
+    expect(gameBoard.placeShipAt(createShip({ length: 2 }), { x: 0, y: 6 })).toBe(true);
+    expect(gameBoard.placeShipAt(createShip({ length: 2 }), { x: 0, y: 8 })).toBe(true);
+    expect(gameBoard.placeShipAt(createShip({ length: 2 }), { x: 6, y: 0 })).toBe(true);
+    expect(gameBoard.placeShipAt(createShip({ length: 1 }), { x: 6, y: 2 })).toBe(true);
+    expect(gameBoard.placeShipAt(createShip({ length: 1 }), { x: 6, y: 4 })).toBe(true);
+    expect(gameBoard.placeShipAt(createShip({ length: 1 }), { x: 6, y: 6 })).toBe(true);
+    expect(gameBoard.placeShipAt(createShip({ length: 1 }), { x: 6, y: 8 })).toBe(true);
+
+    expect(gameBoard.isAllShipsSunk()).toBe(false);
+    expect(gameBoard.getAliveShipsCount()).toBe(10);
+
+    // sink 4len ship
+    expect(gameBoard.receiveAttack({ x: 0, y: 0 })).toBe(true);
+    expect(gameBoard.receiveAttack({ x: 1, y: 0 })).toBe(true);
+    expect(gameBoard.receiveAttack({ x: 2, y: 0 })).toBe(true);
+    expect(gameBoard.receiveAttack({ x: 3, y: 0 })).toBe(true);
+
+    expect(gameBoard.isAllShipsSunk()).toBe(false);
+    expect(gameBoard.getAliveShipsCount()).toBe(9);
+
+    // sink 3len ships
+    expect(gameBoard.receiveAttack({ x: 0, y: 2 })).toBe(true);
+    expect(gameBoard.receiveAttack({ x: 1, y: 2 })).toBe(true);
+    expect(gameBoard.receiveAttack({ x: 2, y: 2 })).toBe(true);
+
+    expect(gameBoard.receiveAttack({ x: 0, y: 4 })).toBe(true);
+    expect(gameBoard.receiveAttack({ x: 1, y: 4 })).toBe(true);
+    expect(gameBoard.receiveAttack({ x: 2, y: 4 })).toBe(true);
+
+    // sink 2len ships
+
+    expect(gameBoard.receiveAttack({ x: 0, y: 6 })).toBe(true);
+    expect(gameBoard.receiveAttack({ x: 1, y: 6 })).toBe(true);
+
+    expect(gameBoard.receiveAttack({ x: 0, y: 8 })).toBe(true);
+    expect(gameBoard.receiveAttack({ x: 1, y: 8 })).toBe(true);
+
+    expect(gameBoard.receiveAttack({ x: 6, y: 0 })).toBe(true);
+    expect(gameBoard.receiveAttack({ x: 7, y: 0 })).toBe(true);
+
+    // sink 1len ships
+    expect(gameBoard.receiveAttack({ x: 6, y: 2 })).toBe(true);
+    expect(gameBoard.receiveAttack({ x: 6, y: 4 })).toBe(true);
+    expect(gameBoard.receiveAttack({ x: 6, y: 6 })).toBe(true);
+    expect(gameBoard.receiveAttack({ x: 6, y: 8 })).toBe(true);
+
+    expect(gameBoard.isAllShipsSunk()).toBe(true);
+    expect(gameBoard.getAliveShipsCount()).toBe(0);
   });
 });
