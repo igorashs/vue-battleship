240c5c6e365877a6af454a7dafcddaa9cac2a50c
igorashs <igorashsirbu@gmail.com>
2019-11-15 14:33:04 +0200

    add nav bar

---
+++ src/components/TheNavBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav>
+    <h1 class="logo">Battleship</h1>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+nav {
+  height: 100px;
+  background-color: rgb(61, 61, 61);
+}
+
+nav h1 {
+  text-align: center;
+  font-family: bfont;
+  font-size: 4rem;
+  text-shadow: 0 2px 2px black;
+}
+</style>
