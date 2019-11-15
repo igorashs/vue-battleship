b2a50f1bcc3af3bd0f5878c86c13224b8080cfeb
igorashs <igorashsirbu@gmail.com>
2019-11-15 18:26:50 +0200

    add game menu

---
+++ src/components/TheGameMenu.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-dialog v-model="isOpen" persistent></v-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    isOpen: Boolean,
+  },
+};
+</script>
+
+<style scoped>
+</style>
