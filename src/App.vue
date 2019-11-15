66b86b4c9558b8ee7b8195e02838a4040d00cea0
igorashs <igorashsirbu@gmail.com>
2019-11-15 14:33:36 +0200

    add reset css rules

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,42 @@
 <template>
   <v-app>
-    <v-content> </v-content>
+    <TheNavBar />
+    <v-content></v-content>
   </v-app>
 </template>
 
 <script>
+import TheNavBar from './components/TheNavBar.vue';
+
 export default {
   name: 'App',
+
+  components: {
+    TheNavBar,
+  },
 
   data: () => ({
     //
   }),
 };
 </script>
+
+<style>
+@font-face {
+  font-family: bfont;
+  src: url('./assets/fonts/04B_30__.TTF');
+}
+
+*,
+*::before,
+*::after {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-size: inherit;
+}
+
+html {
+  font-size: 10px;
+}
+</style>
