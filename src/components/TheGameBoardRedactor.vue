9a82b55afc8ee32fad7053c6880fb48ea985c0fe
igorashs <igorashsirbu@gmail.com>
2019-11-16 18:21:30 +0200

    add container

--- src/components/TheGameBoardRedactor.vue
+++ src/components/TheGameBoardRedactor.vue
@@ -1,5 +1,17 @@
 <template>
-  <v-dialog v-model="isOpen" persistent> </v-dialog>
+  <v-dialog v-model="isOpen" persistent>
+    <div class="container">
+      <h2>Arrange your board</h2>
+      <div class="redactor">
+        <div class="board"></div>
+        <div class="ships-selection"></div>
+      </div>
+      <div class="options">
+        <button>Random</button>
+        <button>Next</button>
+      </div>
+    </div>
+  </v-dialog>
 </template>
 
 <script>
@@ -11,4 +23,29 @@ export default {
 </script>
 
 <style scoped>
+.container h2 {
+  font-size: 2.4rem;
+  font-family: bfont;
+  text-shadow: 0 2px 2px black;
+  text-align: center;
+  text-decoration: underline;
+}
+
+.options {
+  display: flex;
+  justify-content: space-around;
+}
+
+.options button {
+  font-size: 2rem;
+  font-family: bfont;
+  text-shadow: 0 2px 2px black;
+  text-align: center;
+  outline: none;
+}
+
+.options button:hover,
+.options button:focus {
+  color: rgb(128, 255, 0);
+}
 </style>
