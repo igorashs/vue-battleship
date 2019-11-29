53b2876acfd241d841f8e19c84af0bcbeed5d257
igorashs <igorashsirbu@gmail.com>
2019-11-30 01:43:51 +0200

    add updatePcBoard and styles for pc board

--- src/components/TheGame.vue
+++ src/components/TheGame.vue
@@ -43,9 +43,45 @@ export default {
     addPcBoardEvent() {
       this.pcBoardElement.addEventListener('click', (e) => {
         if (e.target.classList.contains('spot')) {
-          this.$emit('round', JSON.parse(e.target.dataset.cord));
+          this.$emit('round', e.target.dataset.cord);
         }
       });
+    },
+
+    updatePcBoard(cord, response) {
+      const spot = this.pcBoardElement
+        .querySelector(`.spot[data-cord=${JSON.stringify(cord)}]`);
+
+      if (response === true) {
+        spot.append('x');
+        spot.style.backgroundColor = 'rgb(248, 39, 39)';
+        spot.style.lineHeight = '1';
+      }
+
+      if (response === false) {
+        spot.append('*');
+      }
+
+      if (response.ship) {
+        const ship = document.createElement('div');
+        ship.classList.add('ship');
+
+        for (let i = 0; i < response.ship.getLength(); i += 1) {
+          const part = document.createElement('div');
+          part.classList.add('part');
+          part.append('x');
+          ship.appendChild(part);
+        }
+
+        const firstSpot = this.pcBoardElement
+          .querySelector(`.spot[data-cord=${JSON.stringify(JSON.stringify(response.cords[0]))}]`);
+
+        ship.style['grid-auto-flow'] = response.isVertical ? 'row' : 'column';
+        ship.style.position = 'absolute';
+
+        if (firstSpot.firstChild) firstSpot.firstChild.remove();
+        firstSpot.appendChild(ship);
+      }
     },
   },
 };
@@ -72,8 +108,30 @@ export default {
 
 .pc >>> .spot {
   cursor: crosshair;
+  text-shadow: 0 0 2px black;
+  text-align: center;
+  font-size: var(--spot-size);
 }
 .pc >>> .spot:hover {
   background-color: rgb(98, 0, 255);
+}
+
+.pc >>> .spot .ship {
+  display: grid;
+  grid-auto-flow: column;
+  background-color: rgb(187, 82, 82);
+  grid-gap: 4px;
+  z-index: 69;
+}
+
+.pc >>> .spot .part {
+  width: var(--spot-size);
+  height: var(--spot-size);
+  background-color: rgb(218, 100, 100);
+  border: 4px solid rgb(70, 70, 70);
+  text-shadow: 0 0 2px black;
+  text-align: center;
+  font-size: var(--spot-size);
+  line-height : 0.745;
 }
 </style>
